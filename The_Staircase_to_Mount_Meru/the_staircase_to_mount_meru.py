1d8c0d79c4181132c004937ddcd9ccfaa088213f
James <jtmaher2@gmail.com>
2019-05-05 20:15:11 -0500

    staircase

--- The_Staircase_to_Mount_Meru/the_staircase_to_mount_meru.py
+++ The_Staircase_to_Mount_Meru/the_staircase_to_mount_meru.py
@@ -1,53 +1,38 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
+import sys
 import math
 
-# |0|1|0|
-# 0|1|1|0
-# |1|2|1|
-# 1|3|3|1
 # generate Pascal's triangle up to a certain level
 def gen_triangle(level):
-    if level == 0:
-        return [[1]]
-    
+    # calculate triangle width based on level
+    num_digits_in_row = (level + 1) * 2 + 1
     tri = [] # array is the # of levels tall, and 3 + the # of levels wide
-    num_digits_in_row = level * 2 + 1
+    
     for l in range(0, level + 1):
-        tri2 = []
-        # total no. of zeros in row is height - dist. from current row to highest row
-        
-        num_zeros = level * ((level - l) + 1)
-        num_zeros_on_each_side = int(num_zeros / 2)
+        if l == 0:
+            # row is simply a bunch of 0s, with a 1 in the middle
+            row = []
+            for i in range(0, num_digits_in_row):
+                if (i != math.floor(num_digits_in_row / 2)):
+                    row.append(0)
+                else:
+                    row.append(1)
+        else:
+            # level > 0
+            row = []
+            prev_row = tri[l - 1]
 
-        # pad left side with zeros
-        for z in range(0, num_zeros_on_each_side):
-            tri2.append(0)
-        
-        # add middle numbers
-        for m in range(0, num_digits_in_row - num_zeros):
-            
-            if l == 0:
-                # if this was the first row, add 1
-                tri2.append(1)
-            else:
-                # find numbers in prev. row that are diagonal from this number
-                prev_row = tri[l - 1]
-                # there is a previous row, so add the sum of the two diagonal digits
-                tri2.append(prev_row[num_zeros_on_each_side + m - 1] + prev_row[num_zeros_on_each_side + m + 1])
-            
-            # in either case, add a zero after the just-added digit
-            #tri2.append(0)
+            for i in range(0, num_digits_in_row):
+                row.append(prev_row[i - 1] + prev_row[i])
 
-        # pad right side with zeros
-        for z in range(0, int(num_zeros_on_each_side)):
-            tri2.append(0)
+            row.append(0) # add trailing 0
 
-        tri.append(tri2) # add row to triangle grid
+        tri.append(row) # add row to triangle grid
 
     return tri
 
-with open('sample.txt') as file_input:
+with open(sys.argv[1]) as file_input:
     for line in file_input:
         parts = line.split(' ')
         level = int(parts[0])
